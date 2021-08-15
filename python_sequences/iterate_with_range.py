e6eee5f761f0771fc712c43fd708c5d1f87296b0
margaret1100 <margaret1100@users.noreply.github.com>
2021-08-15 15:18:34 -0500

    Create iterate_with_range.py

---
+++ python_sequences/iterate_with_range.py
@@ -0,0 +1,9 @@
+#use range to iterate over numbers
+#range[start, stop, step]
+#ranges exclusive of stop values -  will not include stop value
+for i in range(0, 10, 1):
+  print(i)
+  
+#example can be shorted to stop value
+for i in range(10):
+  print(i)
