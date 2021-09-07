e16b42ebe0a11103d1d6bb613cc641bbb768f9bd
margaret1100 <margaret1100@users.noreply.github.com>
2021-09-06 19:07:50 -0500

    Create built-in emulation.py

---
+++ Object_Oriented/built-in emulation.py
@@ -0,0 +1,16 @@
+class Inventory:
+    def __init__(self):
+        self.slots = []
+        
+    def add(self, item):
+        self.slots.append(item)
+        
+    def __len__(self):
+        return len(self.slots)
+    
+    def __contains__(self, item):
+        return item in self.slots 
+    
+    def __iter__(self):
+        #lets you get values and keep working - a generator
+        yield from self.slots
